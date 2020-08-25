accb56d53c9da6948669f8dd08e555e7bc921ae3
mag700 <69214300+mag700@users.noreply.github.com>
2020-08-25 10:53:24 -0400

    Updated one command to rule them all
    Addition: One command to rule them all, One command to find it, and One command to Run it! 
    powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JUU6G')"

--- Win10EsportStream.ps1
+++ Win10EsportStream.ps1
@@ -12,7 +12,7 @@
 #
 #	Addition: One command to rule them all, One command to find it, and One command to Run it! 
 #
-#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JJAEN')"
+#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JUU6G')"
 #
 #	Chris Titus Additions:
 #
