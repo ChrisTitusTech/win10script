2fdc7c1365643d4e51a5da59d727084ed2354f55
Chris Titus <dfm.titus@gmail.com>
2020-11-03 16:36:46 -0600

    Restore Scripts Addition

--- restore-ac-tray.ps1
+++ restore-ac-tray.ps1
@@ -1,12 +1,5 @@
 # This script restores cortana, action center and tray icons to their out of box state
 
-$tweaks = @(
-	### Require administrator privileges ###
-	"RequireAdmin",
-    "EnableCortana",
-    "HideTrayIcons",
-    "EnableActionCenter"
-)
 Function RequireAdmin {
     If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
         Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
@@ -36,25 +29,5 @@ Function EnableCortana {
 	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -ErrorAction SilentlyContinue
 }
 
-##########
-# Parse parameters and apply tweaks
-##########
+RequireAdmin
 
-# Normalize path to preset file
-$preset = ""
-$PSCommandArgs = $args
-If ($args -And $args[0].ToLower() -eq "-preset") {
-	$preset = Resolve-Path $($args | Select-Object -Skip 1)
-	$PSCommandArgs = "-preset `"$preset`""
-}
-
-# Load function names from command line arguments or a preset file
-If ($args) {
-	$tweaks = $args
-	If ($preset) {
-		$tweaks = Get-Content $preset -ErrorAction Stop | ForEach { $_.Trim() } | Where { $_ -ne "" -and $_[0] -ne "#" }
-	}
-}
-
-# Call the desired tweak functions
-$tweaks | ForEach { Invoke-Expression $_ }
