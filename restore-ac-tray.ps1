2f33ed830fa58f79057bfe3454083586a37f4e56
Chris Titus <dfm.titus@gmail.com>
2020-11-04 10:00:25 -0600

    Full GUI Implementation

--- restore-ac-tray.ps1
+++ restore-ac-tray.ps1
@@ -1,4 +1,8 @@
 # This script restores cortana, action center and tray icons to their out of box state
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
 $ErrorActionPreference = 'SilentlyContinue'
 
 $Button = [System.Windows.MessageBoxButton]::YesNoCancel
@@ -24,21 +28,56 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     }
 }
 
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = New-Object System.Drawing.Point(600,400)
+$Form.text                       = "Form"
+$Form.TopMost                    = $false
 
+$PictureBox1                     = New-Object system.Windows.Forms.PictureBox
+$PictureBox1.width               = 600
+$PictureBox1.height              = 45
+$PictureBox1.location            = New-Object System.Drawing.Point(0,14)
+$PictureBox1.imageLocation       = "https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ctt-restore-scripts.png"
+$PictureBox1.SizeMode            = [System.Windows.Forms.PictureBoxSizeMode]::zoom
+$EActionCenter                  = New-Object system.Windows.Forms.Button
+$EActionCenter.text             = "Enable Action Center"
+$EActionCenter.width            = 200
+$EActionCenter.height           = 30
+$EActionCenter.location         = New-Object System.Drawing.Point(50,100)
+$EActionCenter.Font             = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-Function EnableActionCenter {
-	Write-Output "Enabling Action Center..."
+$ECortana                       = New-Object system.Windows.Forms.Button
+$ECortana.text                  = "Enable Cortana"
+$ECortana.width                 = 200
+$ECortana.height                = 30
+$ECortana.location              = New-Object System.Drawing.Point(50,150)
+$ECortana.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$HTrayIcons                     = New-Object system.Windows.Forms.Button
+$HTrayIcons.text                = "Hide Tray Icons"
+$HTrayIcons.width               = 200
+$HTrayIcons.height              = 30
+$HTrayIcons.location            = New-Object System.Drawing.Point(350,100)
+$HTrayIcons.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$Form.controls.AddRange(@($PictureBox1,$EActionCenter,$ECortana,$HTrayIcons))
+
+$HTrayIcons.Add_Click({ 
+	$ErrorActionPreference = 'SilentlyContinue'
+	Write-Host "Hiding tray icons..."
+	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "EnableAutoTray" -ErrorAction SilentlyContinue 
+	Write-Host "Done - Reverted to Stock Settings"
+})
+	
+$EActionCenter.Add_Click({ 
+	Write-Host "Enabling Action Center..."
 	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -ErrorAction SilentlyContinue
-	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -ErrorAction SilentlyContinue
-}
+	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -ErrorAction SilentlyContinue 
+	Write-Host "Done - Reverted to Stock Settings"
+})
 
-Function HideTrayIcons {
-	Write-Output "Hiding tray icons..."
-	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "EnableAutoTray" -ErrorAction SilentlyContinue
-}
-
-Function EnableCortana {
-	Write-Output "Enabling Cortana..."
+$ECortana.Add_Click({ 
+	Write-Host "Enabling Cortana..."
 	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -ErrorAction SilentlyContinue
 	If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore")) {
 		New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
@@ -46,5 +85,8 @@ Function EnableCortana {
 	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 0
 	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 0
 	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -ErrorAction SilentlyContinue
-	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -ErrorAction SilentlyContinue
-}
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -ErrorAction SilentlyContinue 
+	Write-Host "Done - Reverted to Stock Settings"
+})
+
+[void]$Form.ShowDialog()
