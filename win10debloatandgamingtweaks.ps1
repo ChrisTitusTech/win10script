15c075c0833c51052f1cc0403624f8c0c3146eeb
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2020-11-25 05:28:38 +0200

    Update win10debloatandgamingtweaks.ps1

--- win10debloatandgamingtweaks.ps1
+++ win10debloatandgamingtweaks.ps1
@@ -277,14 +277,35 @@ Function InstallTitusProgs {
 
 # Install Microsoft visual C++ Liberaries from 2005 to 2019
 Function InstallMVC {
-Write-Output "Installing the latest Microsoft Visual C++ 2010-2019 Redistributable Packages..."
-choco install -y vcredist2005
-choco install -y vcredist2008
-choco install -y vcredist2010
-choco install -y vcredist2012
-choco install -y vcredist2013
-choco install -y vcredist140
-choco install -y silverlight
+	do
+ {
+    Clear-Host
+    Write-Host "================ Do you want to Install the latest Microsoft Visual C++ 2005-2019 Redistributable Packages and Silverlight? ================"
+    Write-Host "Y: Press 'Y' to do this."
+    Write-Host "N: Press 'N' to skip this."
+	Write-Host "Q: Press 'Q' to stop the entire script."
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+    'y' { 
+	Write-Output "Installing the latest Microsoft Visual C++ 2010-2019 Redistributable Packages and Silverlight..."
+        choco install -y vcredist2005
+        choco install -y vcredist2008
+        choco install -y vcredist2010
+        choco install -y vcredist2012
+        choco install -y vcredist2013
+        choco install -y vcredist140
+        choco install -y silverlight	
+	}
+    'n' {
+        Write-Output "Cancelled! Installing Required Only vcredist2010 for Timer Resolution service..."
+	choco install -y vcredist2010 | Out-Null
+		}
+    'q' { Exit  }
+    }
+ }
+ until ($selection -match "y" -or $selection -match "n" -or $selection -match "q")
+	
 }
 
 Function InstallAdobe {
