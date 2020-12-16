2a6a52a8dbba98910b6fc0b76b7983526cc9bac7
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2020-12-16 04:01:41 +0200

    Update win10debloatandgamingtweaks.ps1

--- win10debloatandgamingtweaks.ps1
+++ win10debloatandgamingtweaks.ps1
@@ -2826,8 +2826,11 @@ Function FixURLext {
     Write-Host "Fixing White Games Shortcuts created by game launchers...."
     choco install -y setuserfta | Out-Null
     Start-Sleep -s 5
+    Push-Location
+    set-location "$env:ProgramData\chocolatey\lib\setuserfta\tools\SetUserFTA\"
     SetUserFTA.exe del .url | Out-Null
     SetUserFTA.exe .url, InternetShortcut | Out-Null
+    Pop-Location
     choco uninstall -y setuserfta | Out-Null
 }
     
