1d9801802c0b821849f7297609a0fbf595f369ec
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2020-12-17 10:19:24 +0200

    Update win10debloatandgamingtweaks.ps1

--- win10debloatandgamingtweaks.ps1
+++ win10debloatandgamingtweaks.ps1
@@ -2792,11 +2792,14 @@ Function DisableDMA {
 #Disable Process and Kernel Mitigations
 Function DisablePKM {
        Write-Output "Disabling Process and Kernel Mitigations..."
+       $errpref = $ErrorActionPreference #save actual preference
+       $ErrorActionPreference = "silentlycontinue"
        ForEach($v in (Get-Command -Name "Set-ProcessMitigation").Parameters["Disable"].Attributes.ValidValues){Set-ProcessMitigation -System -Disable $v.ToString() -ErrorAction SilentlyContinue}
        Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\*\" -Recurse -ErrorAction SilentlyContinue
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" -Name "DisableExceptionChainValidation" -Type DWord -Value 1
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" -Name "KernelSEHOPEnabled" -Type DWord -Value 0
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "EnableCfg" -Type DWord -Value 0
+       $ErrorActionPreference = $errpref #restore previous preference
 }
 
 #Disallow drivers to get paged into virtual memory.
