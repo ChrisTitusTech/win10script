6f1c5a4421155895be91f18c418acf92ca713a99
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2020-12-29 13:04:38 +0200

    Update win10debloatandgamingtweaks.ps1

--- win10debloatandgamingtweaks.ps1
+++ win10debloatandgamingtweaks.ps1
@@ -3228,9 +3228,9 @@ Function AMDGPUTweaks {
 #Optimizing Network and applying Tweaks for no throttle and maximum speed!
 Function NetworkOptimizations {
        Write-Output "Optimizing Network and applying Tweaks for no throttle and maximum speed!..."
-       New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Psched" | Out-Null -ErrorAction SilentlyContinue
-       New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\QoS" | Out-Null -ErrorAction SilentlyContinue
-       New-Item -Path "HKLM:\SOFTWARE\Microsoft\MSMQ\Parameters" | Out-Null -ErrorAction SilentlyContinue
+       New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Psched" -ErrorAction SilentlyContinue | Out-Null
+       New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\QoS" -ErrorAction SilentlyContinue | Out-Null
+       New-Item -Path "HKLM:\SOFTWARE\Microsoft\MSMQ\Parameters" -ErrorAction SilentlyContinue | Out-Null
        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_MAXCONNECTIONSPER1_0SERVER" -Name "explorer.exe" -Type DWord -Value 10
        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_MAXCONNECTIONSPERSERVER" -Name "explorer.exe" -Type DWord -Value 10
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" -Name "LocalPriority" -Type DWord -Value 4
@@ -3240,27 +3240,27 @@ Function NetworkOptimizations {
        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Psched" -Name "NonBestEffortlimit" -Type DWord -Value 0
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\QoS" -Name "Do not use NLA" -Type String -Value "1"
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "Size" -Type DWord -Value 1
-       Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "LargeSystemCache" -Type DWord -Value 0
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "MaxUserPort" -Type DWord -Value 65534
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "TcpTimedWaitDelay" -Type DWord -Value 30
        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "DefaultTTL" -Type DWord -Value 64
        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\MSMQ\Parameters" -Name "TCPNoDelay" -Type DWord -Value 1
        Set-ItemProperty -Path "HKLM:\SYSTEM\ControlSet001\Control\Lsa" -Name "LmCompatibilityLevel" -Type DWord -Value 1
-       Set-NetTCPSetting -SettingName internet -EcnCapability disabled
-       Set-NetOffloadGlobalSetting -Chimney disabled
-       Set-NetTCPSetting -SettingName internet -Timestamps disabled
-       Set-NetTCPSetting -SettingName internet -MaxSynRetransmissions 2
-       Set-NetTCPSetting -SettingName internet -NonSackRttResiliency disabled
-       Set-NetTCPSetting -SettingName internet -InitialRto 2000
-       Set-NetTCPSetting -SettingName internet -MinRto 300
-       Set-NetTCPSetting -SettingName Internet -AutoTuningLevelLocal normal
-       Set-NetTCPSetting -SettingName internet -ScalingHeuristics disabled
-       netsh int tcp set supplemental internet congestionprovider=ctcp
-       Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing enabled
-       Set-NetOffloadGlobalSetting -ReceiveSideScaling enabled
-       Disable-NetAdapterLso -Name *
-       Disable-NetAdapterChecksumOffload -Name *
+       Set-NetTCPSetting -SettingName internet -EcnCapability disabled | Out-Null
+       Set-NetOffloadGlobalSetting -Chimney disabled | Out-Null
+       Set-NetTCPSetting -SettingName internet -Timestamps disabled | Out-Null
+       Set-NetTCPSetting -SettingName internet -MaxSynRetransmissions 2 | Out-Null
+       Set-NetTCPSetting -SettingName internet -NonSackRttResiliency disabled | Out-Null
+       Set-NetTCPSetting -SettingName internet -InitialRto 2000 | Out-Null
+       Set-NetTCPSetting -SettingName internet -MinRto 300 | Out-Null
+       Set-NetTCPSetting -SettingName Internet -AutoTuningLevelLocal normal | Out-Null
+       Set-NetTCPSetting -SettingName internet -ScalingHeuristics disabled | Out-Null
+       netsh int tcp set supplemental internet congestionprovider=ctcp | Out-Null
+       Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing enabled | Out-Null
+       Set-NetOffloadGlobalSetting -ReceiveSideScaling enabled | Out-Null
+       Disable-NetAdapterLso -Name * | Out-Null
+       Disable-NetAdapterChecksumOffload -Name * | Out-Null
        Set-NetAdapterAdvancedProperty -Name * -DisplayName "Energy-Efficient Ethernet" -DisplayValue "Disabled" -ErrorAction SilentlyContinue
+       Set-NetAdapterAdvancedProperty -Name * -DisplayName "Energy Efficient Ethernet" -DisplayValue "Disabled" -ErrorAction SilentlyContinue
        Set-NetAdapterAdvancedProperty -Name * -DisplayName "Green Ethernet" -DisplayValue "Disabled" -ErrorAction SilentlyContinue
        Set-NetAdapterAdvancedProperty -Name * -DisplayName "Power Saving Mode" -DisplayValue "Disabled" -ErrorAction SilentlyContinue
        Set-NetAdapterAdvancedProperty -Name * -DisplayName "Gigabit Lite" -DisplayValue "Disabled" -ErrorAction SilentlyContinue
