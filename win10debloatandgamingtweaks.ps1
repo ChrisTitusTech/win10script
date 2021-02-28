3a220082ee05646678fac25a93db1619141cad09
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2021-02-28 12:32:19 +0200

    Update win10debloatandgamingtweaks.ps1

--- win10debloatandgamingtweaks.ps1
+++ win10debloatandgamingtweaks.ps1
@@ -3263,6 +3263,17 @@ Function NetworkOptimizations {
         $adapter | Set-NetAdapterPowerManagement
     }
 }
+      $errpref = $ErrorActionPreference #save actual preference
+$ErrorActionPreference = "silentlycontinue"
+$NetworkIDS = @(
+(Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\*").PSChildName
+)
+    foreach ($NetworkID in $NetworkIDS) {
+	Write-Output "Disabling Nagle’s Algorithm..."
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\$NetworkID" -Name "TcpAckFrequency" -Type DWord -Value 1
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\$NetworkID" -Name "TCPNoDelay" -Type DWord -Value 1
+}
+$ErrorActionPreference = $errpref #restore previous preference
        Start-Sleep -s 5
 }
 
