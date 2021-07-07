69996bc4958033c237ff989f08f3f60f9db86e4d
Lisa <justsweetluna@gmail.com>
2021-07-07 17:11:24 +0200

    using uri

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -561,7 +561,7 @@ $sdi.Add_Click({
     # if you find a better way of not hardcoding this url hmu ngl
     $url = "http://sdi-tool.org/releases/sdi_R2102.zip"
     $output = "$PSScriptRoot/dump/sdi.zip"
-    Invoke-WebRequest $url -OutFile $output
+    Invoke-WebRequest -Uri $url -OutFile $output
     New-Item -ItemType directory -Path "$PSScriptRoot/dump/sdi"
     Expand-Archive "$PSScriptRoot/dump/sdi.zip" -DestinationPath "$PSScriptRoot/dump/sdi"
     Start-Sleep -Milliseconds 20000
