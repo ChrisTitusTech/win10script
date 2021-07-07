162ebd9a9e839f6d74952049b56ec275dc05200a
Paul Abib Camano <paul.camano@gmail.com>
2021-07-08 01:24:09 +0800

    Remove Elevation YesNoCancel Prompt code

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -5,25 +5,27 @@ $ErrorActionPreference = 'SilentlyContinue'
 $wshell = New-Object -ComObject Wscript.Shell
 $Button = [System.Windows.MessageBoxButton]::YesNoCancel
 $ErrorIco = [System.Windows.MessageBoxImage]::Error
-$Ask = 'Do you want to run this as an Administrator?
-        Select "Yes" to Run as an Administrator
-        Select "No" to not run this as an Administrator
-
-        Select "Cancel" to stop the script.'
+#$Ask = 'Do you want to run this as an Administrator?
+#        Select "Yes" to Run as an Administrator
+#        Select "No" to not run this as an Administrator
+#
+#        Select "Cancel" to stop the script.'
 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-    $Prompt = [System.Windows.MessageBox]::Show($Ask, "Run as an Administrator or not?", $Button, $ErrorIco)
-    Switch ($Prompt) {
-        #This will debloat Windows 10
-        Yes {
-            Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
-            Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-            Exit
-        }
-        No {
-            Break
-        }
-    }
+#    $Prompt = [System.Windows.MessageBox]::Show($Ask, "Run as an Administrator or not?", $Button, $ErrorIco)
+#    Switch ($Prompt) {
+#        #This will debloat Windows 10
+#        Yes {
+#            Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+#            Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+#            Exit
+#        }
+#        No {
+#            Exit
+#        }
+#    }
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
 }
 
 # GUI Specs
