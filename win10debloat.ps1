8262ef912418812f7cfd4798ead7e1fcaba642a3
Kapilesh Pennichetty <50639146+technocapeman@users.noreply.github.com>
2021-07-27 19:05:45 -0500

    Fixed accidental misspelling in O&O Shutup URL

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -551,7 +551,7 @@ $essentialtweaks.Add_Click({
 
     Write-Host "Running O&O Shutup with Recommended Settings"
     Import-Module BitsTransfer
-    Start-BitsTransfer -Source "https://raw.githubusercontent.com/ChrisTitustech/win10script/master/ooshutup10.cfg" -Destination ooshutup10.cfg
+    Start-BitsTransfer -Source "https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ooshutup10.cfg" -Destination ooshutup10.cfg
     Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
     ./OOSU10.exe ooshutup10.cfg /quiet
 
