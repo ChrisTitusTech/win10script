4dfdd3f0205795025cfbfb4c82f9b23c90b94bb8
Hayden Plumley <mrhaydendp@gmail.com>
2022-04-10 15:19:07 -0700

    Fixed and Re-Introduced Automated Winget Install
    - Added architecture check to install x86 or x64 VCLibs dependency
    - Removed the need for installing installation files
    - Installs without errors on fresh install of Windows 10 & 11

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -14,9 +14,15 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 if (!(Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe)){
     Write-Host "Winget not Found, Installing Now..."
     $ResultText.text = "`r`n" +"`r`n" + "Installing Winget... Please Wait"
+    # Get Architecture Type & Install Latest Version of VCLibs From MS Docs
+    $arch = "x64"
+        if ((Get-WmiObject -Class Win32_ComputerSystem).SystemType -match '(x86)'){
+            $arch = "x86"
+    }
+    $vclibs = (Invoke-WebRequest -UseBasicParsing -URI "https://docs.microsoft.com/en-us/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge").Links.Href | Select-String "Microsoft.VCLibs.$arch"
+    Add-AppxPackage -Path "$vclibs"
     $latest = (Invoke-WebRequest -UseBasicParsing -URI "https://github.com/microsoft/winget-cli/releases/latest").Links.Href | Select-String ".msixbundle"
-    Start-BitsTransfer "https://github.com$latest"; .\Microsoft.DesktopAppInstaller_*.msixbundle
-    $nid = (Get-Process AppInstaller).Id; Wait-Process -Id $nid
+    Add-AppxPackage -Path "https://github.com$latest"
     Write-Host "Winget Successfully Installed"
     $ResultText.text = "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task"
 }
