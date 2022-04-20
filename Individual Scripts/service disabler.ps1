ca61ca21dc6b475bfa71b79f227cc1e080caa354
Calvin Judy <calvin@swiftnode.net>
2022-04-20 16:23:02 -0700

    Update service disabler.ps1

--- Individual Scripts/service disabler.ps1
+++ Individual Scripts/service disabler.ps1
@@ -14,9 +14,9 @@ $services = @(
     "SharedAccess"                                 # Internet Connection Sharing (ICS)
     "TrkWks"                                       # Distributed Link Tracking Client
     "WbioSrvc"                                     # Windows Biometric Service (required for Fingerprint reader / facial detection)
-    #"WlanSvc"                                     # WLAN AutoConfig
+    #"WlanSvc"                                      # WLAN AutoConfig
     "WMPNetworkSvc"                                # Windows Media Player Network Sharing Service
-    #"wscsvc"                                      # Windows Security Center Service
+    #"wscsvc"                                       # Windows Security Center Service
     "WSearch"                                      # Windows Search
     "XblAuthManager"                               # Xbox Live Auth Manager
     "XblGameSave"                                  # Xbox Live Game Save Service
@@ -37,7 +37,7 @@ $services = @(
     "PrintNotify"                                  # Disables Windows printer notifications and extentions
     "PcaSvc"                                       # Disables Program Compatibility Assistant Service
     "WPDBusEnum"                                   # Disables Portable Device Enumerator Service
-    #"LicenseManager"                              # Disable LicenseManager(Windows store may not work properly)
+    #"LicenseManager"                               # Disable LicenseManager(Windows store may not work properly)
     "seclogon"                                     # Disables Secondary Logon(disables other credentials only password will work)
     "SysMain"                                      # Disables sysmain
     "lmhosts"                                      # Disables TCP/IP NetBIOS Helper
@@ -45,12 +45,12 @@ $services = @(
     "FontCache"                                    # Disables Windows font cache
     "RetailDemo"                                   # Disables RetailDemo whic is often used when showing your device
     "ALG"                                          # Disables Application Layer Gateway Service(Provides support for 3rd party protocol plug-ins for Internet Connection Sharing)
-    #"BFE"                                         # Disables Base Filtering Engine (BFE) (is a service that manages firewall and Internet Protocol security)
-    #"BrokerInfrastructure"                        # Disables Windows infrastructure service that controls which background tasks can run on the system.
+    #"BFE"                                          # Disables Base Filtering Engine (BFE) (is a service that manages firewall and Internet Protocol security)
+    #"BrokerInfrastructure"                         # Disables Windows infrastructure service that controls which background tasks can run on the system.
     "SCardSvr"                                     # Disables Windows smart card
     "EntAppSvc"                                    # Disables enterprise application management.
     "BthAvctpSvc"                                  # Disables AVCTP service (if you use  Bluetooth Audio Device or Wireless Headphones. then don't disable this)
-    #"FrameServer"                                 # Disables Windows Camera Frame Server(this allows multiple clients to access video frames from camera devices.)
+    #"FrameServer"                                  # Disables Windows Camera Frame Server(this allows multiple clients to access video frames from camera devices.)
     "Browser"                                      # Disables computer browser
     "BthAvctpSvc"                                  # AVCTP service (This is Audio Video Control Transport Protocol service.)
     "BDESVC"                                       # Disables bitlocker
@@ -59,16 +59,16 @@ $services = @(
     "MicrosoftEdgeElevationService"                # Disables one of edge  service 
     "edgeupdatem"                                  # Disables another one of update service (disables edgeupdatem)                          
     "SEMgrSvc"                                     # Disables Payments and NFC/SE Manager (Manages payments and Near Field Communication (NFC) based secure elements)
-    #"PNRPsvc"                                     # Disables peer Name Resolution Protocol ( some peer-to-peer and collaborative applications, such as Remote Assistance, may not function, Discord will still work)
-    #"p2psvc"                                      # Disables Peer Name Resolution Protocol(nables multi-party communication using Peer-to-Peer Grouping.  If disabled, some applications, such as HomeGroup, may not function. Discord will still work)
-    #"p2pimsvc"                                    # Disables Peer Networking Identity Manager (Peer-to-Peer Grouping services may not function, and some applications, such as HomeGroup and Remote Assistance, may not function correctly.Discord will still work)
+    #"PNRPsvc"                                      # Disables peer Name Resolution Protocol ( some peer-to-peer and collaborative applications, such as Remote Assistance, may not function, Discord will still work)
+    #"p2psvc"                                       # Disables Peer Name Resolution Protocol(nables multi-party communication using Peer-to-Peer Grouping.  If disabled, some applications, such as HomeGroup, may not function. Discord will still work)
+    #"p2pimsvc"                                     # Disables Peer Networking Identity Manager (Peer-to-Peer Grouping services may not function, and some applications, such as HomeGroup and Remote Assistance, may not function correctly.Discord will still work)
     "PerfHost"                                     # Disables remote users and 64-bit processes to query performance.
     "BcastDVRUserService_48486de"                  # Disables GameDVR and Broadcast is used for Game Recordings and Live Broadcasts
     "CaptureService_48486de"                       # Disables ptional screen capture functionality for applications that call the Windows.Graphics.Capture API.  
     "cbdhsvc_48486de"                              # Disables cbdhsvc_48486de (clipboard service it disables)
     "BluetoothUserService_48486de"                 # Disables BluetoothUserService_48486de (The Bluetooth user service supports proper functionality of Bluetooth features relevant to each user session.)
     "WpnService"                                   # Disables WpnService (Push Notifications may not work )
-    #"StorSvc"                                     # Disables StorSvc (usb external hard drive will not be reconised by windows)
+    #"StorSvc"                                      # Disables StorSvc (usb external hard drive will not be reconised by windows)
     "RtkBtManServ"                                 # Disables Realtek Bluetooth Device Manager Service
     "QWAVE"                                        # Disables Quality Windows Audio Video Experience (audio and video might sound worse)
     # HP Services
