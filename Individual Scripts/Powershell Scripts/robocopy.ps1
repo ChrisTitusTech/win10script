4748730736dd607a7e335fccb2374a54c309e0a1
Carterpersall <carterpersall@gmail.com>
2022-04-23 18:36:45 -0500

    Automatic CPU thread detection
    Added automatic CPU thread detection into robocopy.ps1

--- Individual Scripts/Powershell Scripts/robocopy.ps1
+++ Individual Scripts/Powershell Scripts/robocopy.ps1
@@ -1,5 +1,13 @@
-﻿
+﻿#Gets CPU Logical Processor Count
+$CPUs = (wmic cpu get NumberOfLogicalProcessors | Select-String -NotMatch NumberOfLogicalProcessors)
+$num = 0
+foreach($line in $CPUs){
+    if($num -eq 1){
+        $CPUs = $line
+    }
+    $num++
+}
 
 $S = Read-Host -Prompt 'Input Source Directory'
 $D = Read-Host -Prompt 'Input Destination Directory'
-robocopy $S $D /e /w:5 /r:2 /COPY:DATSOU /DCOPY:DAT /MT
+robocopy $S $D /e /w:5 /r:2 /COPY:DATSOU /DCOPY:DAT /MT:$CPUs
