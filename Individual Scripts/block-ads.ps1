5d5709fcbd85a5697eac87edaeb4355901b0d860
Carterpersall <carterpersall@gmail.com>
2022-04-23 17:39:58 -0500

    Sort Individual Scripts into folders
     - Sorted all scripts(excluding win10debloat.ps1) into separate folders

--- Individual Scripts/block-ads.ps1
+++
@@ -1,3 +0,0 @@
-$adlist= Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Brandonbr1/ad-list-hosts/main/host' 
-$adfile = "$env:windir\System32\drivers\etc\hosts"
-$adlist | Add-Content -PassThru $adfile
