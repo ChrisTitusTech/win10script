13b7f28565f211a0fd33cb2bcc33d635c2553e64
Carterpersall <carterpersall@gmail.com>
2022-04-23 18:35:46 -0500

    Add link to creator's repo in block-ads.ps1

--- Individual Scripts/Powershell Scripts/block-ads.ps1
+++ Individual Scripts/Powershell Scripts/block-ads.ps1
@@ -1,3 +1,3 @@
-$adlist= Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Brandonbr1/ad-list-hosts/main/host' 
+$adlist= Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Brandonbr1/ad-list-hosts/main/host' # Repo: https://github.com/Brandonbr1/ad-list-hosts/
 $adfile = "$env:windir\System32\drivers\etc\hosts"
 $adlist | Add-Content -PassThru $adfile
