59b0d9af15c599cf3606e29c94261a7fb3405d9e
Hayden Plumley <mrhaydendp@gmail.com>
2022-05-17 13:40:06 -0700

    Updated Comment to be Clearer
    - Clearer comment
    - Updated Get-Process with WinStore.App

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -13,11 +13,11 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 # GUI Specs
 Write-Host "Checking for Winget..."
 
-# If Winget isn't Installed, Open MS Store Window to Install
+# If Winget isn't Installed, Open App Installer Store Page to Install
 if (!(Get-AppPackage -name 'Microsoft.DesktopAppInstaller')) {
     Write-Host "Winget not Found, Installing Now"
     Start-Process "ms-windows-store://pdp/?ProductId=9NBLGGH4NNS1"
-    $nid = (Get-Process AppInstaller).Id; Wait-Process -Id $nid
+    $nid = (Get-Process WinStore.App).Id; Wait-Process -Id $nid
     Write-Host "Winget Successfully Installed"
 }
 
